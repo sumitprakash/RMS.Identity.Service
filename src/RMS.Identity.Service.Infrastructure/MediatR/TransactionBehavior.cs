9453022ff08382c03777c75f01aea9f678517cd9
sumitprakash <sumit_prakash@outlook.com>
2026-02-07 04:09:02 +0530

    wip

--- src/RMS.Identity.Service.Infrastructure/MediatR/TransactionBehavior.cs
+++ src/RMS.Identity.Service.Infrastructure/MediatR/TransactionBehavior.cs
@@ -1,36 +1,33 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using MediatR;
+﻿using MediatR;
 using RMS.Identity.Service.Infrastructure.Data;
+using RMS.Identity.Service.Application.Commands;
 
-namespace RMS.Identity.Service.Infrastructure.MediatR
+public class TransactionBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
 {
-    public class TransactionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    private readonly IUnitOfWork _uow;
+
+    public TransactionBehavior(IUnitOfWork uow)
     {
-        private readonly IUnitOfWork _uow;
+        _uow = uow;
+    }
 
-        public TransactionBehavior(IUnitOfWork uow) => _uow = uow;
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (request is not ITransactionalCommand)
+            return await next();
 
-        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        _uow.Begin();
+        try
         {
-            // If request is not a transactional command, just execute
-            if (request is not RMS.Identity.Service.Application.Commands.ITransactionalCommand)
-                return await next();
-
-            // Begin transaction, execute handler, commit/rollback
-            _uow.Begin();
-            try
-            {
-                var response = await next();
-                _uow.Commit();
-                return response;
-            }
-            catch
-            {
-                _uow.Rollback();
-                throw;
-            }
+            var response = await next();
+            _uow.Commit();
+            return response;
+        }
+        catch
+        {
+            _uow.Rollback();
+            throw;
         }
     }
 }
